c9a1e005bf36a4c1bf296cce850c887942bc0849
jiyaaany <jiyaaany@gmail.com>
2021-06-04 21:44:11 +0900

    [feat] styled-components 로 버튼과 원 그리기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled, { css } from 'styled-components';
+import Button from './components/Button';
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color};
+  border-radius: 50%;
+  ${props => 
+    props.huge && 
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBlock>
+      <Circle color="blue" />
+      <Circle color="blue" huge />
+      <Button>BUTTON</Button>
+    </AppBlock>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import { darken, lighten } from 'polished';
+
+const StyledButton = styled.button`
+  display: inline-block;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  height: 2.25rem;
+  font-size: 1rem;
+
+  background: #228be6;
+  &:hover {
+    background: ${lighten(0.1, '#228be6')};
+  }
+  &:active {
+    background: ${darken(0.1, '#228be6')};
+  }
+
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+function Button({ children, ...rest }) {
+  return <StyledButton {...rest}>{children}</StyledButton>;
+}
+
+export default Button;
